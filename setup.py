191000025f7a1e1f4547e564cdf9604146041316
vladstreltsin <vladstreltsin@gmail.com>
2021-02-24 12:26:03 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="blox",
+
+    python_requires='>=3.7',
+
+    version="0.0.1",
+
+    author="Vlad Streltsin",
+
+    author_email="vladstreltsin@gmail.com",
+
+    description="Block diagram modeling",
+
+    long_description=long_description,
+
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/vladstreltsin/blox.git",
+
+    find_packages=['blox'],
+
+    install_requires=[
+    ],
+
+    extras_require={
+    }
+)
